a570e85065151687f1f630690b4e0efc64efefcd
firafara <106955105+firafara@users.noreply.github.com>
2023-01-16 10:00:11 +0700

    update api service

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -26,16 +26,13 @@ class APIService {
 
 //mengambil dari API laravel
     var data = jsonDecode(response.body) as Map<String, dynamic>;
-    // print(data);
     var user = data['user'] as Map<String, dynamic>;
-    // print('user_id : ' + data['user'].toString());
     int userId = user['id'];
     var auth = data['auth'] as Map<String, dynamic>;
     var token = auth['token'] as String;
 
-    // print(userId);
     addUserIdToSF(userId);
-    // print(await getUIDFromSF());
+    print(await getUIDFromSF());
     addAPITokenToSF(token);
     // print('user_id_fromSF : ' + UserID.toString());
     if (response.statusCode == 200) {

a3842550db5996ab55c24b5db42207b06b27b863
firafara <106955105+firafara@users.noreply.github.com>
2023-01-16 09:59:14 +0700

    update config
    mengubah api url

--- lib/config.dart
+++ lib/config.dart
@@ -1,6 +1,6 @@
 class Config {
   static const String appName = "Creative";
-  static const String apiURL = "192.168.4.245:8000";
+  static const String apiURL = "creative.multicraneperkasa.com";
   static const String loginAPI = "/api/login";
   static const String apiSPK = "/api/service/forms/spk";
 }
